38b44196d1ddc890477c9117edcdf118051073b2
leaprovenzano <leaprovenzano@gmail.com>
2017-06-06 14:29:55 +0100

    genome handler now uses functional API and is to my mind much cleaner. moved all metric and objective things to do with loss and accuracy to devol object, added docstrings cleaned up population scoring and fixed some little bugs i had created.. added PReLU and LeakyReLU which are just called with defaults for now.

--- genome_handler.py
+++ genome_handler.py
@@ -1,10 +1,12 @@
 import numpy as np
 import random as rand
 import math
-from keras.models import Sequential
-from keras.layers import Activation, Dense, Dropout, Flatten, GlobalAveragePooling2D, GlobalMaxPooling2D
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.models import Sequential, Model
+from keras.layers import Activation, Dense, Dropout, Flatten, GlobalAveragePooling2D, GlobalMaxPooling2D, Input
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU, PReLU
+
 from keras.optimizers import SGD
 from tqdm import tqdm
 
@@ -24,12 +26,11 @@ from tqdm import tqdm
 class GenomeHandler:
     def __init__(self, max_conv_layers, max_dense_layers, max_filters, max_dense_nodes,
                  input_shape, n_classes, batch_normalization=True, dropout=True, max_pooling=True,
-                 optimizers=None, activations=None, metric='accuracy'):
+                 optimizers=None, activations=None):
         if max_dense_layers < 1:
             raise ValueError(
                 "At least one dense layer is required for softmax layer")
-        filter_range_max = int(math.log(max_filters, 2)) + \
-            1 if max_filters > 0 else 0
+        filter_range_max = int(math.log(max_filters, 2)) + int(max_filters > 0)
         self.optimizer = optimizers or [
             'adam',
             'rmsprop',
@@ -44,6 +45,8 @@ class GenomeHandler:
             'relu',
             'elu',
             'sigmoid',
+            LeakyReLU,
+            PReLU
         ]
         self.convolutional_layer_shape = [
             # Present
@@ -57,7 +60,7 @@ class GenomeHandler:
             # Dropout
             [(i if dropout else 0) for i in range(11)],
             # Max Pooling
-            list(range(3)) if max_pooling else 0,
+            list(range(3)) if max_pooling else [0],
         ]
         self.dense_layer_shape = [
             # Present
@@ -81,12 +84,12 @@ class GenomeHandler:
         self.dense_layer_size = len(self.dense_layer_shape)
         self.input_shape = input_shape
         self.n_classes = n_classes
-        self.metric = metric
 
     def mutate(self, genome, num_mutations):
         num_mutations = np.random.choice(num_mutations)
         for i in range(num_mutations):
             index = np.random.choice(list(range(1, len(genome))))
+
             if index < self.convolution_layer_size * self.convolution_layers:
                 if genome[index - index % self.convolution_layer_size]:
                     range_index = index % self.convolution_layer_size
@@ -94,8 +97,14 @@ class GenomeHandler:
                     genome[index] = np.random.choice(choice_range)
                 elif rand.uniform(0, 1) <= 0.01:  # randomly flip deactivated layers
                     genome[index - index % self.convolution_layer_size] = 1
+
+            elif index == (self.convolution_layer_size * self.convolution_layers) + 1:
+                genome[index] = np.random.choice(
+                    [v for v in range(len(self.flatten_layers)) if v != genome[index]])
+
             elif index != len(genome) - 1:
-                offset = self.convolution_layer_size * self.convolution_layers
+                offset = (self.convolution_layer_size *
+                          self.convolution_layers) + 1
                 new_index = (index - offset)
                 present_index = new_index - new_index % self.dense_layer_size
                 if genome[present_index + offset]:
@@ -109,59 +118,83 @@ class GenomeHandler:
                     list(range(len(self.optimizer))))
         return genome
 
+    def build_conv(self, encoding, kernal_size=(3, 3), scale_drop=.05):
+        """build a basic convolutional layer according
+        to parameters defined in encoding.
+        """
+        _, filters, bn, activation_index, drop, pool = encoding
+        activation = self.activation[activation_index]
+
+        def block(inp):
+            x = Conv2D(filters, kernal_size, padding='same')(inp)
+            if bn:
+                x = BatchNormalization()(x)
+
+            # a hacky check for advanced activations
+            if type(activation) is not str:
+                x = activation()(x)
+            else:
+                x = Activation(activation)(x)
+            x = Dropout(drop * scale_drop)(x)
+            if pool == 1:
+                x = MaxPooling2D(pool_size=(2, 2), padding="same")(x)
+
+            elif pool == 2: #pool using convolution with stride of 2
+                x = Conv2D(filters, kernal_size, strides=(
+                    2, 2), padding='same')(inp)
+            return x
+        return block
+
+    def build_dense(self, encoding, scale_drop=.07):
+        """build a basic dense layer according
+        to parameters defined in encoding.
+        """
+        _, nodes, bn, activation_index, drop = encoding
+        activation = self.activation[activation_index]
+
+        def block(inp):
+            x = Dense(nodes)(inp)
+            if bn:
+                x = BatchNormalization()(x)
+            # a hacky check for advanced activations
+            if type(activation) is not str:
+                x = activation()(x)
+            else:
+                x = Activation(activation)(x)
+            x = Dropout(drop * scale_drop)(x)
+            return x
+
+        return block
+
     def decode(self, genome):
         if not self.is_compatible_genome(genome):
             raise ValueError("Invalid genome for specified configs")
-        model = Sequential()
-        offset = 0
-        input_layer = True
-        for i in range(self.convolution_layers):
-            if genome[offset]:
-                convolution = None
-                if input_layer:
-                    convolution = Convolution2D(
-                        genome[offset + 1], (3, 3),
-                        padding='same',
-                        input_shape=self.input_shape)
-                    input_layer = False
-                else:
-                    convolution = Convolution2D(
-                        genome[offset + 1], (3, 3),
-                        padding='same')
-                model.add(convolution)
-                if genome[offset + 2]:
-                    model.add(BatchNormalization())
-                _activation = self.activation[genome[offset + 3]]
-                model.add(Activation(_activation))
-                model.add(Dropout(float(genome[offset + 4] / 20.0)))
-                max_pooling_type = genome[offset + 5]
-                if max_pooling_type == 1:
-                    model.add(MaxPooling2D(pool_size=(2, 2), padding="same"))
-            offset += self.convolution_layer_size
+            
+        # initilize input layer
+        input_layer = Input(shape=self.input_shape)
+        x = input_layer  # set cursor to input layer so we don't have to bother checking
 
-        if not input_layer:
-            model.add(genome[offset])  # add flatten or global pool layer
-            offset += 1
+        # add conv layers
+        for i in range(0, self.convolution_layers * self.convolution_layer_size, self.convolution_layer_size):
+            params = genome[i:i + self.convolution_layer_size]
+            if params[0]:
+                x = self.build_conv(params)(x)
 
-        for i in range(self.dense_layers):
-            if genome[offset]:
-                dense = None
-                if input_layer:
-                    dense = Dense(genome[offset + 1],
-                                  input_shape=self.input_shape)
-                    input_layer = False
-                else:
-                    dense = Dense(genome[offset + 1])
-                model.add(dense)
-                if genome[offset + 2]:
-                    model.add(BatchNormalization())
-                model.add(Activation(self.activation[genome[offset + 3]]))
-                model.add(Dropout(float(genome[offset + 4] / 20.0)))
-            offset += self.dense_layer_size
+        # add out flatten or global pooling layer
+        ix = (self.convolution_layers * self.convolution_layer_size) + 1
+        x = self.flatten_layers[genome[ix]](x)
 
-        model.add(Dense(self.n_classes, activation='softmax'))
+        # add dense layers
+        for i in range(ix, ix + (self.dense_layer_size * self.dense_layers), self.dense_layer_size):
+            params = genome[i:i + self.dense_layer_size]
+            if params[0]:
+                x = self.build_dense(params)(x)
+        output = Dense(self.n_classes, activation='softmax')(x)
+
+        # initilize functional api model & compile
+        model = Model(inputs=input_layer, outputs=output)
         model.compile(loss='categorical_crossentropy',
-                      optimizer=self.optimizer[genome[offset]],
+                      optimizer=self.optimizer[genome[-1]],
                       metrics=["accuracy"])
         return model
 
@@ -170,7 +203,7 @@ class GenomeHandler:
         for i in range(self.convolution_layers):
             for r in self.convolutional_layer_shape:
                 genome.append(np.random.choice(r))
-        genome.append(np.random.choice(self.flatten_layers))
+        genome.append(np.random.choice(range(len(self.flatten_layers))))
         for i in range(self.dense_layers):
             for r in self.dense_layer_shape:
                 genome.append(np.random.choice(r))
@@ -189,20 +222,23 @@ class GenomeHandler:
                 if genome[ind + j] not in self.convolutional_layer_shape[j]:
                     return False
             ind += self.convolution_layer_size
-        ind+=1 #adding room for flatten  or global pooling layer
+        ind += 1  # adding room for flatten  or global pooling layer
+        if genome[ind] not in range(len(self.flatten_layers)):
+            return False
         for i in range(self.dense_layers):
             for j in range(self.dense_layer_size):
                 if genome[ind + j] not in self.dense_layer_shape[j]:
                     return False
             ind += self.dense_layer_size
+
         if genome[ind] not in range(len(self.optimizer)):
             return False
         return True
 
     # metric = accuracy or loss
-    def best_genome(self, csv_path, include_metrics=True):
-        best = max if self.metric is "accuracy" else min
-        col = -1 if self.metric is "accuracy" else -2
+    def best_genome(self, csv_path, metric='accuracy', include_metrics=True):
+        best = max if metric is "accuracy" else min
+        col = -1 if metric is "accuracy" else -2
         data = np.genfromtxt(csv_path, delimiter=",")
         row = list(data[:, col]).index(best(data[:, col]))
         genome = list(map(int, data[row, :-2]))
